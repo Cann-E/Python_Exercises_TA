e2e4b50a064f196668966e9e20adb90832dc36d6
Cann-E <cann0211@gmail.com>
2025-02-06 14:03:00 -0600

    initial commit

--- Dictionaries_3.py
+++ Dictionaries_3.py
@@ -60,12 +60,76 @@ print(dict_6)
 
 # Write a program that creates a dictionary, copies it, and checks if they have the same contents but different memory locations.
 
+dict_8={"name":"sarah","age":"30","major":"Cs"}
+
+copy_8=dict_8.copy()
+
+if copy_8 == dict_8 and id(copy_8) != id(dict_8):
+    print("Copies have same values but different memory locations.")
+else:
+    print("Something is wrong!")
 
 
 # Merge two dictionaries and remove a key from the merged dictionary.
 
+dict_9={
+    "name":"can",
+    "age":21,
+    "gender":"m"
+}
+
+dict_10={"name_2":"mike","age_2":99,"gender_2":"f"}
+
+merged_dict= dict_9 | dict_10
+merged_dict.pop("name")
+print(merged_dict)
+
 # Merge two dictionaries and print only the common keys.
+
+dict_11={"name":"mike","age":25,"gender":"M"}
+
+dict_12={
+    "name":"charlie",
+    "make":"maserati",
+    "model":"ghibli",
+    "year":2020
+}
+
+common_keys = set(dict_11.keys()) & set(dict_12.keys()) 
+print(common_keys)
+merged_dict_1=dict_11 | dict_12
+print(merged_dict_1)
 
 # Create two dictionaries and find keys that exist in both.
 
+dict_13={"name":"mike","age":25,"gender":"M"}
+
+dict_14={
+    "name":"charlie",
+    "make":"maserati",
+    "model":"ghibli",
+    "year":2020
+}
+
+common_keys = set(dict_13.keys()) & set(dict_14.keys()) 
+
+if common_keys:
+    print(f"Common Keys {common_keys}")
+else:
+    print("No common!")
+
+
 # Find the difference between two dictionaries (keys that exist in one but not the other).
+
+dict_15={"name":"mike","age":25,"gender":"M"}
+
+dict_16={
+    "name":"charlie",
+    "make":"maserati",
+    "model":"ghibli",
+    "year":2020
+}
+
+diff_keys=set(dict_15.keys()) ^ set(dict_16.keys())
+
+print (diff_keys)

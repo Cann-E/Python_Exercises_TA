709b45706fda322326ee977e31bca1fdb66a639c
Cann-E <cann0211@gmail.com>
2025-02-25 11:58:06 -0600

    initial commit

--- Dict_Exec.py
+++ Dict_Exec.py
@@ -138,17 +138,293 @@ student.update_grade("asli",100)
 # Implement a method remove_student(name) that removes a student from the dictionary.
 # If the student does not exist, return "Student not found".
 
+class Student():
+    def __init__(self):
+        self.names_dict={}
+        
+    def add_student(self,new_student,grade):
+        self.names_dict[new_student]=grade
+        print(f"Student added:Name{new_student},Grade:{grade}")
+        
+    def get_grade(self,name):
+        found=False
+        for student in self.names_dict:
+            if student == name:
+                found=True
+                return self.names_dict[student]
+        if not found:
+            print("Student not found!")
+    
+    def has_student(self,name):
+        for student in self.names_dict:
+            if student == name:
+                return True
+        else:
+            return False
+        
+    def update_grade(self,name,new_grade):
+        for student in self.names_dict:
+            if student ==name:
+                self.names_dict[student]=new_grade
+                print(f"Student:{student} new grade:{new_grade}")
+                found=True
+                break
+        if not found:
+            print(f"Student:{student} not found")
+            
+    def remove_student(self,name):
+        for student in self.names_dict:
+            if student ==name:
+                self.names_dict.pop(name)
+                print(f"Student Removed:{name}")
+                found=True
+                break
+        if not found:
+            print(f"Student not found:{name}")
+         
+                
+student=Student()
+student.add_student("can",95)
+student.add_student("asli",85)
+
+print(student.get_grade("can"))
+print(student.get_grade("asli"))
+
+print(student.has_student("can"))
+print(student.has_student("mike"))
+
+student.update_grade("asli",100)
+
+student.remove_student("can")
+
+
+
 
 
 # 6-Count the Number of Entries in the Dictionary
 # Add a method count_students() that returns the number of students in the dictionary.
 
+class Student():
+    def __init__(self):
+        self.names_dict={}
+        
+    def add_student(self,new_student,grade):
+        self.names_dict[new_student]=grade
+        print(f"Student added:Name{new_student},Grade:{grade}")
+        
+    def get_grade(self,name):
+        found=False
+        for student in self.names_dict:
+            if student == name:
+                found=True
+                return self.names_dict[student]
+        if not found:
+            print("Student not found!")
+    
+    def has_student(self,name):
+        for student in self.names_dict:
+            if student == name:
+                return True
+        else:
+            return False
+        
+    def update_grade(self,name,new_grade):
+        for student in self.names_dict:
+            if student ==name:
+                self.names_dict[student]=new_grade
+                print(f"Student:{student} new grade:{new_grade}")
+                found=True
+                break
+        if not found:
+            print(f"Student:{student} not found")
+            
+    def remove_student(self,name):
+        for student in self.names_dict:
+            if student ==name:
+                self.names_dict.pop(name)
+                print(f"Student Removed:{name}")
+                found=True
+                break
+        if not found:
+            print(f"Student not found:{name}")
+    
+    def count_students(self):
+        return f"Student Count: {(len(self.names_dict.keys()))}"
+         
+                
+student=Student()
+student.add_student("can",95)
+student.add_student("asli",85)
+student.add_student("mike",10)
+student.add_student("charlie",24)
+
+print(student.get_grade("can"))
+print(student.get_grade("asli"))
+
+print(student.has_student("can"))
+print(student.has_student("mike"))
+
+student.update_grade("asli",100)
+
+student.remove_student("can")
+
+print(student.count_students())
+
+
 # 7-Return All Keys and Values
 # Implement a method get_all_students() that returns all student names and grades as a dictionary.
+
+class Student():
+    def __init__(self):
+        self.names_dict={}
+        
+    def add_student(self,new_student,grade):
+        self.names_dict[new_student]=grade
+        print(f"Student added:Name{new_student},Grade:{grade}")
+        
+    def get_grade(self,name):
+        found=False
+        for student in self.names_dict:
+            if student == name:
+                found=True
+                return self.names_dict[student]
+        if not found:
+            print("Student not found!")
+    
+    def has_student(self,name):
+        for student in self.names_dict:
+            if student == name:
+                return True
+        else:
+            return False
+        
+    def update_grade(self,name,new_grade):
+        for student in self.names_dict:
+            if student ==name:
+                self.names_dict[student]=new_grade
+                print(f"Student:{student} new grade:{new_grade}")
+                found=True
+                break
+        if not found:
+            print(f"Student:{student} not found")
+            
+    def remove_student(self,name):
+        for student in self.names_dict:
+            if student ==name:
+                self.names_dict.pop(name)
+                print(f"Student Removed:{name}")
+                found=True
+                break
+        if not found:
+            print(f"Student not found:{name}")
+    
+    def count_students(self):
+        return f"Student Count: {(len(self.names_dict.keys()))}"
+    
+    def get_all_students(self):
+        return self.names_dict.items()
+         
+                
+student=Student()
+student.add_student("can",95)
+student.add_student("asli",85)
+student.add_student("mike",10)
+student.add_student("charlie",24)
+
+print(student.get_grade("can"))
+print(student.get_grade("asli"))
+
+print(student.has_student("can"))
+print(student.has_student("mike"))
+
+student.update_grade("asli",100)
+
+student.remove_student("can")
+
+print(student.count_students())
+
+print(student.get_all_students())
+
 
 # 8-Find the Student with the Highest Grade
 # Implement a method top_student() that returns the name of the student with the highest grade.
 # If no students are present, return "No students available".
+
+class Student():
+    def __init__(self):
+        self.names_dict={}
+        
+    def add_student(self,new_student,grade):
+        self.names_dict[new_student]=grade
+        print(f"Student added:Name{new_student},Grade:{grade}")
+        
+    def get_grade(self,name):
+        found=False
+        for student in self.names_dict:
+            if student == name:
+                found=True
+                return self.names_dict[student]
+        if not found:
+            print("Student not found!")
+    
+    def has_student(self,name):
+        for student in self.names_dict:
+            if student == name:
+                return True
+        else:
+            return False
+        
+    def update_grade(self,name,new_grade):
+        for student in self.names_dict:
+            if student ==name:
+                self.names_dict[student]=new_grade
+                print(f"Student:{student} new grade:{new_grade}")
+                found=True
+                break
+        if not found:
+            print(f"Student:{student} not found")
+            
+    def remove_student(self,name):
+        for student in self.names_dict:
+            if student ==name:
+                self.names_dict.pop(name)
+                print(f"Student Removed:{name}")
+                found=True
+                break
+        if not found:
+            print(f"Student not found:{name}")
+    
+    def count_students(self):
+        return f"Student Count: {(len(self.names_dict.keys()))}"
+    
+    def get_all_students(self):
+        return self.names_dict.items()
+    
+    def top_student(self):
+        return max(self.names_dict.items())
+         
+                
+student=Student()
+student.add_student("can",95)
+student.add_student("asli",85)
+student.add_student("mike",10)
+student.add_student("charlie",24)
+
+print(student.get_grade("can"))
+print(student.get_grade("asli"))
+
+print(student.has_student("can"))
+print(student.has_student("mike"))
+
+student.update_grade("asli",100)
+
+student.remove_student("can")
+
+print(student.count_students())
+
+print(student.get_all_students())
+
+print(student.top_student())
 
 # 9-Reverse a Dictionary (Swap Keys and Values)
 # Implement a method reverse_records() that swaps the student names with their grades, making the grades the keys and names the values.

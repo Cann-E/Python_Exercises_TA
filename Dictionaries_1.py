5007bfe1689d03ac923d54a9ff7aec91d83dd5ed
Cann-E <cann0211@gmail.com>
2025-03-07 12:23:07 -0600

    initial commit

--- Dictionaries_1.py
+++ Dictionaries_1.py
@@ -41,7 +41,7 @@ print(dict_5.items())#1
 # Check if a specific key exists in a dictionary using an if statement.
 dict_6={
     "book":"Harry Potter",
-    "author":"JK rowlings"#1122
+    "author":"JK rowlings"#11223
 }
 
 if "author" in dict_6:
